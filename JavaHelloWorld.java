a7a4122a04f6b3785da1d403bccc51fd9ec09585
Pavithra Holla <paviholla@gmail.com>
2015-11-18 14:27:45 -0700

    Third set

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -1,6 +1,7 @@
 public class JavaHelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Automating Build on Doker hub");
     }
 }
 
